94807b7d5d40c11e0d1b96d204826b43d83cd6fc
DongMing Hu <821312@ACDM.DS.SAIT.CA>
2019-03-28 20:07:54 -0600

    Index page showing all assets.

--- CPRG214.Assets.BLL/AssetManager.cs
+++ CPRG214.Assets.BLL/AssetManager.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CPRG214.Assets.Data;
+using CPRG214.Assets.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace CPRG214.Assets.BLL
 {
     public class AssetManager
     {
+        public static List<Asset> GetAllAssets()
+        {
+            var context = new AssetsContext();
+            return context.Assets.
+                Include(a => a.AssetType).
+                ToList();
+        }
     }
 }
